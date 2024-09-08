06291f4ee88228a68860ce4a86fe73343ddf2760
VeraCB <bogosveronica98'gmail.com>
2024-09-08 01:01:01 +0100

    3 exercises done

---
+++ src/js/01-gallery.js
@@ -0,0 +1,46 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items.js';
+
+import SimpleLightbox from 'simplelightbox';
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+import '../css/common.css';
+import '../css/01-gallery.css';
+
+// Change code below this line
+
+const galleryContainerEl = document.querySelector(".gallery");
+const imagesMarkup = createItemsMarkup(galleryItems);
+galleryContainerEl.insertAdjacentHTML("beforeend", imagesMarkup);
+
+function createItemsMarkup(item) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+      <a class="gallery__link" href="${original.value}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`;
+    })
+    .join("");
+}
+const onContainerClick = (e) => {
+  e.preventDefault();
+
+  if (e.target.classList.contains("gallery")) return;
+    const source = e.target.dataset.source;
+    
+  const instance = basicLightbox.create(`
+    <img src="${source}"width="800" height="600">`);
+
+  instance.show();
+};
+
+galleryContainerEl.addEventListener("click", onContainerClick);
